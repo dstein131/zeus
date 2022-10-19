0fbbfaf3645eced8ba8d1e647c9b636496447bcd
David Stein <dstein131@gmail.com>
2022-10-19 15:41:08 -0400

    register buttons etc

--- preLoginNav.js
+++ preLoginNav.js
@@ -44,7 +44,7 @@ function renderNavBar() {
       <div class="collapse navbar-collapse" id="navbarText">
       <div class="ms-auto">
       <button class="btn btn-primary" type="submit">Login</button>
-      <button class="btn btn-secondary" type="submit">Register</button>
+      <button class="btn btn-success" type="submit">Register</button>
       </div>
       
         </div>
